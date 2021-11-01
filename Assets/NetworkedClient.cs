6062b3fa4fb38a2f357a549a928e7bc5ba069d94
Tyler <phantomxbla@gmail.com>
2021-11-01 09:16:03 -0400

    Forgot to commit changes from "simplest client assignment" from when I had to host a server. Changed IP to public IP

--- Assets/NetworkedClient.cs
+++ Assets/NetworkedClient.cs
@@ -12,7 +12,7 @@ public class NetworkedClient : MonoBehaviour
     int reliableChannelID;
     int unreliableChannelID;
     int hostID;
-    int socketPort = 5491;
+    int socketPort = 27943;
     byte error;
     bool isConnected = false;
     int ourClientID;
@@ -79,7 +79,7 @@ public class NetworkedClient : MonoBehaviour
             hostID = NetworkTransport.AddHost(topology, 0);
             Debug.Log("Socket open.  Host ID = " + hostID);
 
-            connectionID = NetworkTransport.Connect(hostID, "192.168.0.160", socketPort, 0, out error); // server is local on network
+            connectionID = NetworkTransport.Connect(hostID, "99.236.146.202", socketPort, 0, out error); // server is local on network
 
             if (error == 0)
             {
